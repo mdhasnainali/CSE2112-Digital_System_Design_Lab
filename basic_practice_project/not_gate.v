a2279e924549d1f4f4bc9b3710851b4927f0e5d1
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2022-09-05 23:58:32 +0600

    Adds .gitignore file for modelsim

---
+++ basic_practice_project/not_gate.v
@@ -0,0 +1,9 @@
+module not_gate(in, out); // module_name(inputs, outputs)
+
+// Defining input output and wires
+input in;
+output out;
+
+// Define circuit functionality
+assign out = ~in;
+endmodule
