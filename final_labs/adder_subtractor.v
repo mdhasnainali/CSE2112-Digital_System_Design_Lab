07bd61d401e972b0b59b26ff2710559cf5868775
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2022-11-22 22:37:45 +0600

    Adder Subtractor

---
+++ final_labs/adder_subtractor.v
@@ -0,0 +1,17 @@
+module adder_subtractor(Carry, SUM, M, A, B);
+
+input [3:0] A,B;
+input M;
+output [3:0] SUM;
+output Carry;
+
+wire [3:0] Bbar;
+
+assign Bbar[0] = B[0] ^ M,
+	Bbar[1] = B[1] ^ M,
+	Bbar[2] = B[2] ^ M,
+	Bbar[3] = B[3] ^M ;
+
+assign {Carry, SUM} = A + Bbar + M;
+
+endmodule
