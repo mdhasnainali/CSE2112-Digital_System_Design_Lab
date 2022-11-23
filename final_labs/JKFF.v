8193202224135859bc47aa3cc981a43c162414e6
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2022-11-23 15:29:04 +0600

    mod 10 counter added

---
+++ final_labs/JKFF.v
@@ -0,0 +1,18 @@
+module JKFF(Q,Qbar,J,K,CLR,CLK);
+
+input J,K,CLK,CLR;
+output Q,Qbar;
+
+wire a, b, c, d, y, ybar;
+
+// Dataflow Modeling
+assign a = ~(Qbar & J * CLK & CLR),
+	b = ~(Q & K & CLK),
+	y = ~(a & ybar),
+	ybar = ~(y & CLR & b),
+	c = ~(y & ~CLK),
+	d = ~(ybar & ~CLK),
+	Q = ~(c & Qbar),
+	Qbar = ~(Q & CLR & d);
+
+endmodule
---
+++ final_labs/mod10Counter.v
@@ -0,0 +1,19 @@
+module mod10Counter(Q, CLK);
+
+input CLK;
+output [3:0]Q;
+
+wire [3:0]Qbar;
+wire CLR;
+
+assign J = 1,
+	K = 1;
+
+JKFF jk1(Q[0],Qbar[0],J,K,CLR,CLK);
+JKFF jk2(Q[1],Qbar[1],J,K,CLR,Q[0]);
+JKFF jk3(Q[2],Qbar[2],J,K,CLR,Q[1]);
+JKFF jk4(Q[3],Qbar[3],J,K,CLR,Q[2]);
+
+assign CLR = ~(Q[1] & Q[3]);
+
+endmodule
