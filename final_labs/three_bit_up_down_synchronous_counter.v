113ecd644bd9f4d5d6ecc610993d10a9ac12d6ab
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2022-11-23 16:43:18 +0600

    up/down counter added

---
+++ final_labs/three_bit_up_down_synchronous_counter.v
@@ -0,0 +1,21 @@
+module up_down_synchronous_counter(Q,M,CLR,CLK);
+
+input M, CLR, CLK;
+output [2:0] Q;
+
+wire [2:0] J,K,Qbar;
+
+assign J[0] = (~M & ~Q[1] & ~Q[2]) + (M & Q[1] & Q[2]),
+	K[0] =  J[0],
+	J[1] = ~(M ^ Q[2]),
+	K[1] = J[1],
+	J[2] = 1,
+	K[2] = 1;
+
+JKFF jk1(Q[0],Qbar[0],J[0],K[0],CLR,CLK);
+JKFF jk2(Q[1],Qbar[1],J[1],K[1],CLR,CLK);
+JKFF jk3(Q[2],Qbar[2],J[2],K[2],CLR,CLK);
+
+endmodule;
+
+
